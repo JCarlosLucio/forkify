7ece7ed2788678833b69e51b74f8bc81b473ed13
jcLucio <c.lucio3@gmail.com>
2020-06-14 17:03:23 -0500

    Add no-console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
+    'no-console': 'off',
     // 'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };
