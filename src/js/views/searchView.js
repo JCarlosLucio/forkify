dc9d5673c451e4b1c3c52744f7e4c1c4620091c9
jcLucio <c.lucio3@gmail.com>
2020-06-15 23:16:49 -0500

    Add getInput and import elements from base.js

---
+++ src/js/views/searchView.js
@@ -0,0 +1,4 @@
+// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.value;
