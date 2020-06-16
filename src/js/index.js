af987a5df778182f2b883e4670b52d1c30b2e442
jcLucio <c.lucio3@gmail.com>
2020-06-15 22:38:31 -0500

    Import Search

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 // Global app controller
 // URL for API: http://forkify-api.herokuapp.com/
+
+import Search from './models/Search';
+
+const search = new Search('pizza');
+
+console.log(search);
+search.getResults();
