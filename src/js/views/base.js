df32d17c3ea22767c3b5ddb74b87ce6343cd6ad7
jcLucio <c.lucio3@gmail.com>
2020-06-16 00:32:37 -0500

    Add elementStrings

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,9 +5,13 @@ export const elements = {
   searchResList: document.querySelector('.results__list'),
 };
 
+export const elementStrings = {
+  loader: 'loader',
+};
+
 export const renderLoader = (parent) => {
   const loader = `
-    <div class="loader">
+    <div class="${elementStrings.loader}">
       <svg>
         <use href="img/icons.svg#icon-cw"></use>
       </svg>
