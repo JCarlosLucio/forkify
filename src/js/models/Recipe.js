05c7ac519e4200d20b9ae6a48b55d9b636d657a0
jcLucio <c.lucio3@gmail.com>
2020-06-18 19:41:37 -0500

    Add calcTime method to Recipe

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -19,4 +19,11 @@ export default class Recipe {
       console.log(error);
     }
   }
+
+  calcTime() {
+    // Assuming that we need 15 mins for each 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
 }
