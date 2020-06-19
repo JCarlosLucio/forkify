88243d359863449b88b57a8faaddb7028429fe6d
jcLucio <c.lucio3@gmail.com>
2020-06-18 19:07:32 -0500

    Add getRecipe method to Recipe class

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -4,4 +4,16 @@ export default class Recipe {
   constructor(id) {
     this.id = id;
   }
+
+  async getRecipe() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+      );
+      this.result = res;
+      console.log(this.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
