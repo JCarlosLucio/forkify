c96b040fd3a8f457d485337c83ba455446cc19b7
jcLucio <c.lucio3@gmail.com>
2020-06-23 16:19:46 -0500

    Add hightlightSelected to searchView

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,17 @@ export const clearResults = () => {
   elements.searchResPages.textContent = '';
 };
 
+export const highlightSelected = (id) => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+  resultsArr.forEach((el) => {
+    el.classList.remove('results__link--active');
+  });
+
+  document
+    .querySelector(`a[href="#${id}"]`)
+    .classList.add('results__link--active');
+};
+
 // 'Pasta with tomato and spinach' example for limitRecipe reduce
 // acc 0 / acc + cur.length = 5 / newTitle = ['Pasta']
 // acc 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
