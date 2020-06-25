ea5894464cae1bc722c99a45118ce3a11b46deda
jcLucio <c.lucio3@gmail.com>
2020-06-25 16:56:57 -0500

    Add addItem method to List class

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -2,4 +2,13 @@ export default class List {
   constructor() {
     this.items = [];
   }
+
+  addItem(count, unit, ingredient) {
+    const item = {
+      id,
+      count,
+      unit,
+      ingredient,
+    };
+  }
 }
