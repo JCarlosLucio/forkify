1fd40c14c1e8aca4498dd6b8ce4c946bebc2d5e7
jcLucio <c.lucio3@gmail.com>
2020-06-29 10:28:31 -0500

    Add addLike method to Likes class

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -2,4 +2,15 @@ export default class Likes {
   constructor() {
     this.likes = [];
   }
+
+  addLike(id, title, author, img) {
+    const like = {
+      id,
+      title,
+      author,
+      img,
+    };
+    this.likes.push(like);
+    return like;
+  }
 }
